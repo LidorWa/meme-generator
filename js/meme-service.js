a44be0557f32c635a082e1cd3ca9934f77bd3c72
Lidor Waldman <lidorvl@gmail.com>
2022-02-19 21:57:47 +0200

    added new funcs:
    * add new line
    * delete line

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,6 +1,9 @@
 'use strict'
 
 // will handle the meme's MODEL
+
+let gSavedMemes = [];
+
 let gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
@@ -30,4 +33,35 @@ let gMeme = {
 
 function getMemeFromService(){
     return gMeme;
+  }
+
+  function createNewLine(txt = '', size = 16, align = 'left', fillColor = 'white', strokeColor = 'black', font = 'IMPACT', posX = 1, posY = 20){
+    return {
+      txt: txt,
+      size: size,
+      align: align,
+      fillColor: fillColor,
+      strokeColor: strokeColor,
+      font: font,
+      posX: posX,
+      posY: posY
+    }
+  }
+
+  function createNewLines(posY){
+  
+    gMeme.lines.push(createNewLine(posY))
+  }
+
+
+  function deleteLines(currLine){
+    if(!gMeme.lines || !gMeme.lines.length) {
+      return;
+    }
+
+    gMeme.lines.splice(currLine, 1);
+  }
+
+  function createSavedMemes(){
+
   }
